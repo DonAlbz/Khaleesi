d44fd7d394bff8d5a9886c4079c4b0c17dfe6e44
Alvi <alberto.vivenzi@gmail.com>
2016-04-25 01:23:19 +0200

    confermo aggiunta DodoBot

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,36 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Preparati Mik.\nL'Inverno sta arrivando.\n Sono pronto!");
+		boolean dodoIsGay = true;
+		//System.out.println("Preparati Mik.\nL'Inverno sta arrivando.\n Sono pronto!");
+		dodoBot(dodoIsGay);
+		
+		
+	}
+	
+	private static void dodoBot(boolean dodoIsGay) {
+		if (dodoIsGay){
+			repeat();
+		}
+		else peace();
+		
 	}
 
+	private static void peace(){
+	//do nothing	
+	}
+	private static void repeat(){
+		String[] paroleFrequenti ={"cazzo", "sbora", "froci di merda", "grosso", "bravo e bello"};
+		try{
+			while(true){
+				System.out.println(paroleFrequenti[(int) (Math.random()*paroleFrequenti.length)]);
+				Thread.sleep(500);
+				}			
+			}
+		catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+			
+		
+	}
 }
